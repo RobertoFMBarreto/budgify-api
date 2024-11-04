dd236f6f41ed40f13659c02b3464c5e6228e8c8b
Roberto Barreto <robertofmbarreto@gmail.com>
2024-11-04 19:11:33 +0000

    file structure

---
+++ auth/Domain/Enums/CustomEnum.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace auth.Domain.Enums
+{
+    public class CustomEnum
+    {
+        
+    }
+}
